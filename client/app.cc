fcd102202d286ae0205b0f0ed7265f46262b93e3
mikelu94 <michaellu94@gmail.com>
2025-08-05 21:53:18 -0400

    Update client proto messages to be arena allocated

--- client/app.cc
+++ client/app.cc
@@ -2,10 +2,12 @@
 #include <memory>
 #include <string>
 
+#include <google/protobuf/arena.h>
 #include <grpcpp/grpcpp.h>
 
 #include "proto/item.grpc.pb.h"
 
+using google::protobuf::Arena;
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::ClientReader;
@@ -26,57 +28,58 @@ int main(int argc, char* argv[]) {
     std::unique_ptr<Itemizer::Stub> stub(Itemizer::NewStub(channel));
     
     ClientContext context;
-    Items items;
-    Item item;
-    NullRequest null_request;
-    SuccessResponse success_response;
-    FoundResponse found_response;
+    Arena arena;
+    Items* items = Arena::Create<Items>(&arena);
+    Item* item = Arena::Create<Item>(&arena);
+    NullRequest* null_request = Arena::Create<NullRequest>(&arena);
+    SuccessResponse* success_response = Arena::Create<SuccessResponse>(&arena);
+    FoundResponse* found_response = Arena::Create<FoundResponse>(&arena);
 
     if (argc > 1) {
         std::string subcommand(argv[1]);
         if (subcommand == "create_item" && argc == 3) {
-            item.set_name(argv[2]);
-            stub->CreateItem(&context, item, &success_response);
+            item->set_name(argv[2]);
+            stub->CreateItem(&context, *item, success_response);
         } else if (subcommand == "create_items" && argc > 2) {
-            for (int i=2; i<argc; ++i) items.add_items()->set_name(argv[i]);
-            stub->CreateItems(&context, items, &success_response);
+            for (int i=2; i<argc; ++i) items->add_items()->set_name(argv[i]);
+            stub->CreateItems(&context, *items, success_response);
         } else if (subcommand == "get_items") {
-            stub->GetItems(&context, null_request, &items);
+            stub->GetItems(&context, *null_request, items);
             std::cout << "[";
-            int item_size = items.items_size();
+            int item_size = items->items_size();
             for (int i=0; i<item_size; ++i) {
-                std::cout << items.items(i).name();
+                std::cout << items->items(i).name();
                 if (i != item_size - 1) std::cout << ",";
             }
             std::cout << "]\n";
         } else if (subcommand == "create_items_stream") {
-            std::unique_ptr<ClientWriter<Item>> writer(stub->CreateItemsStream(&context, &success_response));
+            std::unique_ptr<ClientWriter<Item>> writer(stub->CreateItemsStream(&context, success_response));
             std::string item_name;
             std::cout << "New item name: ";
             while (std::cin >> item_name && item_name.length()) {
-                item.set_name(item_name);
-                writer->Write(item);
+                item->set_name(item_name);
+                writer->Write(*item);
                 std::cout << "New item name: ";
             }
             writer->WritesDone();
             writer->Finish();
         } else if (subcommand == "get_items_stream") {
-            std::unique_ptr<ClientReader<Item>> reader(stub->GetItemsStream(&context, null_request));
-            while (reader->Read(&item)) std::cout << item.name() << "\n";
+            std::unique_ptr<ClientReader<Item>> reader(stub->GetItemsStream(&context, *null_request));
+            while (reader->Read(item)) std::cout << item->name() << "\n";
             reader->Finish();
         } else if (subcommand == "find_items_stream") {
             std::shared_ptr<ClientReaderWriter<Item,FoundResponse>> stream(stub->FindItemsStream(&context));
             std::cout << "item name to query: ";
             std::string item_name;
             std::cin >> item_name;
-            item.set_name(item_name);
-            stream->Write(item);
-            while (stream->Read(&found_response)) {
-                std::cout << (found_response.found() ? "true\n" : "false\n" );
+            item->set_name(item_name);
+            stream->Write(*item);
+            while (stream->Read(found_response)) {
+                std::cout << (found_response->found() ? "true\n" : "false\n" );
                 std::cout << "item name to query: ";
                 std::cin >> item_name;
-                item.set_name(item_name);
-                stream->Write(item);
+                item->set_name(item_name);
+                stream->Write(*item);
             }
         } else {
             print_help();
